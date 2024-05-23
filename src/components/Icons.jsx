5659cc3de0d14f6de50e48388fd88ddf4f6f416d
Isreal Castro <israelcastro.dev@gmail.com>
2024-05-23 10:36:52 -0500

    Iconos

---
+++ src/components/Icons.jsx
@@ -0,0 +1,31 @@
+export function IconPlus() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="50"
+      height="50"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#fff"
+        d="M17 13h-4v4h-2v-4H7v-2h4V7h2v4h4m-5-9A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"
+      />
+    </svg>
+  );
+}
+
+export function IconMinus() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="50"
+      height="50"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#fff"
+        d="M17 13H7v-2h10m-5-9A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"
+      />
+    </svg>
+  );
+}
