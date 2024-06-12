e74db7f1df02231dad305d1bd0903936b98e14a5
Isreal Castro <israelcastro.dev@gmail.com>
2024-06-12 13:13:10 -0500

    Poniendo link del repositorio de Midu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,16 @@ function Footer() {
   return (
     <footer className="w-full bg-slate-900 p-2 text-center text-white mt-2">
       <p>Hecho por Israel Castro - {year}</p>
+      <p>
+        Problema tomado del repositorio de{" "}
+        <a
+          href="https://github.com/midudev/pruebas-tecnicas/tree/main/pruebas/01-reading-list"
+          target="_blank"
+          className="font-black text-sky-700 underline underline-offset-2"
+        >
+          Midudev
+        </a>
+      </p>
     </footer>
   );
 }
